b4eab6e3fb55fc0d585ac6689ae8339b338dd948
Tomas <tslemenson@gmail.com>
2019-08-01 14:19:50 -0700

    Updates

---
+++ precache-manifest.331602ef5653a488bbab01b39f93aed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50e738393d699a4d4e05d45b215b46dc",
+    "url": "/Clicky_Game/index.html"
+  },
+  {
+    "revision": "3b2d27b2a28687ff0231",
+    "url": "/Clicky_Game/static/css/main.0e0fa4cd.chunk.css"
+  },
+  {
+    "revision": "6e6b97d4ee9c005f67b8",
+    "url": "/Clicky_Game/static/js/2.1a79d570.chunk.js"
+  },
+  {
+    "revision": "3b2d27b2a28687ff0231",
+    "url": "/Clicky_Game/static/js/main.724c1234.chunk.js"
+  },
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  }
+]);
