937570f0469bdd6bf885c013435f8afc8575c723
Tomas <tslemenson@gmail.com>
2019-08-01 14:36:12 -0700

    done

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,7 +6,7 @@ const navbar = props => {
   return (
     <div className="logo jumbotron text-center">
       <h1 className="logo">THE CLICKY GAME</h1>
-      <small>Click on one of the cartoons to incrice your score, but carefull, don't click on the same </small><br></br>
+      <small>Click on one of the cartoons to increase your score, but careful, don't click on the same </small><br></br>
       <small>one because you will lose the game. You win when you score 12 points. Good luck!!!</small>
     </div>
   );
